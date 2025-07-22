8ea7abf10e4e8800fbef3aa402927f5dab6bdf7b
Adit Rastogi <aditrastogi6@gmail.com>
2025-07-22 16:18:49 +0530

    server init

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("server is running fine");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req,res) => {
+    res.send("Server is Ready");
+})
+
+app.listen(PORT,() => {
+    console.log(`Server running on : http://localhost:${PORT}`);
+})
