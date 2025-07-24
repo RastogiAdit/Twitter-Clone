2fde7179dda874da4b4d5a6c30e628c0bb20b88b
Adit Rastogi <aditrastogi6@gmail.com>
2025-07-24 18:00:10 +0530

    follow unfollow added notification left

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +18,7 @@ app.use(express.urlencoded({ extended: true })); //to parse urlencoded data
 app.use(cookieParser()); //to parse cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on : http://localhost:${PORT}`);
