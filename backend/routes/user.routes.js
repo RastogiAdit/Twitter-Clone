5d92d468320d5009a8c878d40d5e17f6c8fb9fa4
Adit Rastogi <aditrastogi6@gmail.com>
2025-07-24 22:09:30 +0530

    suggested feature added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getUserProfile, followUnfollowUser } from '../controllers/user.controller.js';
+import { getUserProfile, followUnfollowUser, getSuggestedUsers } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.get("/suggested",getUserProfile);
+router.get("/suggested",protectRoute, getSuggestedUsers);
 // router.post("/update",updateUserProfile);
 
 export default router;
