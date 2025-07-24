3dbb65331e29393a5792273371247439eab2c461
Adit Rastogi <aditrastogi6@gmail.com>
2025-07-24 23:45:33 +0530

    user route complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary"
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -11,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 // console.log(process.env.MONGO_URI);
 
 app.use(express.json()); //to parse req.body
