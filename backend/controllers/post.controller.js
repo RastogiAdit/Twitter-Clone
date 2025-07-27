13588f2ac21b5e3b906a24882f0d027c0ca169c0
Adit Rastogi <aditrastogi6@gmail.com>
2025-07-27 16:46:15 +0530

    like/unlike and comment added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import {v2 as cloudinary} from "cloudinary"
+import Notification from "../models/notification.model.js";
 
 //models
 import Post from "../models/post.model.js";
@@ -35,7 +36,7 @@ export const createPost = async (req,res) => {
 
     } catch (error) {
         console.log("Error in createPost: ", error.message);
-        res.status(500).json({ error: "Internal Server Error" });
+        return res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
@@ -61,6 +62,73 @@ export const deletePost = async (req,res) => {
 
     } catch (error) {
         console.log("Error in deletePost: ", error.message);
-        res.status(500).json({ error: "Internal Server Error" });
+        return res.status(500).json({ error: "Internal Server Error" });
     }
-}
+};
+
+export const commentOnPost = async (req,res) => {
+    try {
+
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if(!text) {
+            //bad request
+            return res.status(400).json({error: "Text feild is required"});
+        }
+        const post = await Post.findById(postId);
+        if(!post) {
+            return res.status(404).json({error: "Post not Found"});
+        }
+
+        const comment = {
+            user: userId,
+            text: text
+        }
+
+        post.comments.push(comment);
+        await post.save();
+
+        return res.status(200).json(post);
+        
+        
+    } catch (error) {
+        console.log("Error in commentOnPost: ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const likeUnlikePost = async (req,res) => {
+    try {
+        const userId = req.user._id;
+        const {id:postId} = req.params;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({error: "Post not found"});
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if(userLikedPost) {
+            //unlike post
+            await Post.updateOne({_id:postId},{$pull: {likes: userId}});
+            return res.status(200).json({message: "Post unliked successfully"});
+        } else{
+            //like post
+            post.likes.push(userId);
+            await post.save();
+
+            const notification = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like"
+            })
+            await notification.save();
+            return res.status(200).json({message: "Post liked successfully"});
+        }
+        
+    } catch (error) {
+        console.log("Error in likeUnlikePost: ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import { createPost, deletePost, commentOnPost, likeUnlikePost } from "../controllers/post.controller.js";
 
 
 const router = express.Router();
 
 //all the paths
 router.post("/create", protectRoute, createPost)
-// router.post("/like/:id", protectRoute, likeUnlikePost)
-// router.post("/comment/:id", protectRoute, commentOnPost)
+router.post("/like/:id", protectRoute, likeUnlikePost)
+router.post("/comment/:id", protectRoute, commentOnPost)
 router.delete("/:id", protectRoute, deletePost)
 
 
