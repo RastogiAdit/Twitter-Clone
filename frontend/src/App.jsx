b4bd19eb77f0b3246a1b10af0f91f8ffb03f8262
Adit Rastogi <aditrastogi6@gmail.com>
2025-08-02 21:36:53 +0530

    protected route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
@@ -11,7 +11,7 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-  const { data,isLoading,error,isError } = useQuery({
+  const { data:authUser,isLoading } = useQuery({
     queryKey: ['authUser'],
     queryFn: async() => {
       try {
@@ -35,17 +35,17 @@ function App() {
     );
   }
   
-  console.log("authUser is here:",data)
+  console.log("authUser is here:",authUser)
 
   return (
     <div className='flex max-w-6xl mx-auto'>
       <SideBar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/notifications' element={<NotificationPage />} />
-        <Route path='/profile/:username' element={<ProfilePage />} />
+        <Route path='/' element={authUser? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/signup' element={!authUser? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/notifications' element={authUser? <NotificationPage /> : <Navigate to="/login" />} />
+        <Route path='/profile/:username' element={authUser? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
       <RightPanel />
       <Toaster />
