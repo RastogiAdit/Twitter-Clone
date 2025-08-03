8353cf2bf11f280a0dde80c805c0b5c2a802bf34
Adit Rastogi <aditrastogi6@gmail.com>
2025-08-03 14:03:36 +0530

    useFollow hook created and used in suggested followers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -39,7 +39,6 @@ export const followUnfollowUser = async (req, res) => {
             //unfollow the user
             await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
             await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
-            // TODO: return the id of the user as a response
             res.status(200).json({ message: "Unfollowed successfully" });
         }
         else
@@ -54,7 +53,6 @@ export const followUnfollowUser = async (req, res) => {
                 to: userToModify._id
             });
             await newNotification.save();
-            // TODO: return the id of the user as a response
             res.status(200).json({ message: "Followed successfully" });
         }
         
