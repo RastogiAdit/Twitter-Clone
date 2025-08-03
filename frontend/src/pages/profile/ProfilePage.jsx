3d935abd52aaef9a1d9825a18e71f304cfbdaa4d
Adit Rastogi <aditrastogi6@gmail.com>
2025-08-03 18:27:48 +0530

    useUpdateUserProfile hook add

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -11,11 +11,11 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { formatMemberSinceDate } from "../../utils/date";
 import useFollow from "../../hooks/useFollow";
-import toast from "react-hot-toast";
+import useUpdateUserProfile from "../../hooks/useUpdateUserProfile";
 
 const ProfilePage = () => {
 	const [coverImg, setCoverImg] = useState(null);
@@ -24,20 +24,19 @@ const ProfilePage = () => {
 
 	const coverImgRef = useRef(null);
 	const profileImgRef = useRef(null);
-	
-	const {username} = useParams();
 
-	const {follow,isPending} = useFollow()
-	const queryClient = useQueryClient();
-	const {data:authUser} = useQuery({queryKey: ["authUser"]});
+	const { username } = useParams();
 
-	const {data:user, isLoading, refetch, isRefetching} = useQuery({
-		queryKey:["userProfile"],
-		queryFn:async() => {
+	const { follow, isPending } = useFollow()
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
+	const { data: user, isLoading, refetch, isRefetching } = useQuery({
+		queryKey: ["userProfile"],
+		queryFn: async () => {
 			try {
 				const res = await fetch(`/api/users/profile/${username}`)
 				const data = await res.json();
-				if(!res.ok) throw new Error(data.error || "Something went wrong");
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
 				return data;
 			} catch (error) {
 				throw new Error(error.message);
@@ -46,37 +45,7 @@ const ProfilePage = () => {
 	})
 
 	//updating cover image and profile image only 
-	const {mutate:updateProfile,isPending:isUpdatingProfile} = useMutation({
-		mutationFn: async () => {
-			try {
-				const res = await fetch(`/api/users/update`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						coverImg,
-						profileImg
-					})
-				});
-				const data = await res.json();
-				if(!res.ok) throw new Error(data.error || "Something went wrong");
-				return data;
-			} catch (error) {
-				throw new Error(error.message);
-			}
-		},
-		onSuccess: () => {
-			toast.success("Profile updated successfully");
-			Promise.all([
-				queryClient.invalidateQueries({queryKey: ["authUser"]}),
-				queryClient.invalidateQueries({queryKey: ["userProfile"]}),
-			])
-		},
-		onError: () => {
-			toast.error(error.message);
-		}
-	})
+	const { isUpdatingProfile, updateProfile } = useUpdateUserProfile();
 
 	const isMyProfile = authUser._id === user?._id;
 	const memberSinceDate = formatMemberSinceDate(user?.createdAt);
@@ -96,7 +65,7 @@ const ProfilePage = () => {
 
 	useEffect(() => {
 		refetch()
-	},[username, refetch]);
+	}, [username, refetch]);
 
 
 	return (
@@ -177,7 +146,11 @@ const ProfilePage = () => {
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => updateProfile()}
+										onClick={async () =>{
+											await updateProfile({ coverImg, profileImg });
+											setProfileImg(null);
+											setCoverImg(null);
+											}}
 									>
 										{isUpdatingProfile ? "Updating..." : "Update"}
 									</button>
@@ -197,12 +170,12 @@ const ProfilePage = () => {
 											<>
 												<FaLink className='w-3 h-3 text-slate-500' />
 												<a
-													href='https://youtube.com/@asaprogrammer_'
+													href={user.link.startsWith('http') ? user.link : `https://${user.link}`}
 													target='_blank'
 													rel='noreferrer'
 													className='text-sm text-blue-500 hover:underline'
 												>
-													youtube.com/@asaprogrammer_
+													{user.link}
 												</a>
 											</>
 										</div>
@@ -246,7 +219,7 @@ const ProfilePage = () => {
 						</>
 					)}
 
-					<Posts feedType={feedType} username={username} userId={user?._id}/>
+					<Posts feedType={feedType} username={username} userId={user?._id} />
 				</div>
 			</div>
 		</>
